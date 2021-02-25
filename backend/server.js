06b3674b871b836fe35a1b39efeff2e93d89ccd5
Thomas-Fletcher <tjfletcher289@gmail.com>
2021-02-25 10:47:41 -0300

    modified:   backend/build/asset-manifest.json 	modified:   backend/build/index.html 	renamed:    backend/build/static/css/main.b4464df3.chunk.css -> backend/build/static/css/main.1b5b6739.chunk.css 	new file:   backend/build/static/css/main.1b5b6739.chunk.css.map 	deleted:    backend/build/static/css/main.b4464df3.chunk.css.map 	deleted:    backend/build/static/js/main.35bbb8ba.chunk.js 	deleted:    backend/build/static/js/main.35bbb8ba.chunk.js.map 	new file:   backend/build/static/js/main.9d3edb72.chunk.js 	new file:   backend/build/static/js/main.9d3edb72.chunk.js.map 	modified:   backend/server.js 	modified:   package-lock.json 	modified:   package.json

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import data from './data.js';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import https from 'https';
 
+const a  = import('https');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
